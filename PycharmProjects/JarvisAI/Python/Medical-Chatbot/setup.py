ce68af6f06580c0a2d96aef05b39af8eb3a227ba
jvpurushotham <pandupuushotham@gmail.com>
2025-06-25 09:44:46 +0530

    Folder structure created

---
+++ PycharmProjects/JarvisAI/Python/Medical-Chatbot/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'J V Purushotham',
+    author_email = 'jvpurushotham31@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
